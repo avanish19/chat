e5e4512f0c39aa5b3e9ab09bd4ed0f300431c13f
nickpolarits <Nikhil@polarits.com>
2017-12-23 17:45:05 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const express = require('express');
+const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname,'../public');
 var app = express();
 
 app.use(express.static(publicPath));
-app.listen(3000,()=>{
-	console.log('Server is up on Port 3000');
 
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
